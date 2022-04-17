8395550b4cecc9e9da3a8102052a656453f9eea5
Vlad <vladvitalaru@yahoo.com>
2022-04-17 14:15:29 -0700

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Vlad Vitalaru on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
